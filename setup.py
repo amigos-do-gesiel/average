2d374f9f6b5ed5597c42e079205664c17b40b14d
Jefersonalves <jefersonalvesf2009@gmail.com>
2017-11-13 14:50:24 -0200

    Update packaging files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 from setuptools import find_packages, setup
 
@@ -8,9 +10,9 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-average',
+    name='average',
     version='0.0.1',
-    packages=find_packages(exclude=("average",)),
+    #packages=find_packages(exclude=("average",)),
     include_package_data=True,
     license='MIT License',  # example license
     description='Framework para acompanhamento e visualização de Estatísticas.',
@@ -18,10 +20,9 @@ setup(
     url='https://github.com/amigos-do-gesiel/average',
     author='amigos-do-gesiel',
     author_email='ulysses3353@gmail.com',
+
+    # Classifiers (see https://pypi.python.org/pypi?%3Aaction=list_classifiers)
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',  # example license
         'Operating System :: OS Independent',
@@ -33,4 +34,25 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+
+    # Packages and dependencies
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    
+    install_requires=[
+        #"django"
+    ],
+    
+    extras_require={
+        'dev': [
+            #'pytest==3.2.3'
+        ],
+    },
+    
+    entry_points = {
+       "console_scripts": ["average = __main__:main"] 
+    },
+    # Other configurations
+    #zip_safe=False,
+    #platforms='any',
 )
