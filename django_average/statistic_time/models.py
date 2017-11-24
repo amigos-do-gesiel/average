84614f33af1607d4778a5fdd1939953bc3c6d272
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-11-24 08:28:00 -0200

    Implementing yearly stats
    Signed-off-by: Marco Kuroiva <marco.antoniorl10@gmail.com>
    Signed-off-by: Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
    Signed-off-by: UlyssesBML <ulysses3353@gmail.com>
    Signed-off-by: Gesiel Freitas <gesiel.was.f@gmail.com>

--- django_average/statistic_time/models.py
+++ django_average/statistic_time/models.py
@@ -7,9 +7,6 @@ class StatisticTime(ObserverStatistic):
     class Meta:
         abstract = True
 
-    def calculate_statistic(self):
-        pass
-
     def get_statistic(self):
         pass
 
@@ -19,17 +16,32 @@ class StatisticTime(ObserverStatistic):
     def remove_statistic_time(self):
         pass
 
+class StatisticYearly(StatisticTime):
+    MONTH_TOTAL = models.IntegerField
+    yearly_value = models.FloatField()
+
+    def update(self, daily_value):
+        self.statistic_year += daily_value
+
+    def add_statistic_time(self, monthly_stat):
+        monthly_stat.stats_year = self
+        self.update(monthly_stat.monthly_value)
+
+    def remove_statistic_time(self, monthly_stat):
+        monthly_stat.stats_year = None
+        self.statistic_year -= monthly_stat.monthly_value
+
+    def get_statistic(self):
+        return self.yearly_value
+
 class StatisticMonthly(StatisticTime):
     # monthly_dontations = models.ForeignKey(StatisticDaily)
     DAY_TOTAL = models.IntegerField() #number of days this month has
-    statistic_month = models.FloatField()
-    #stats_year = models.ForeignKey(StatisticYearly)
+    monthly_value = models.FloatField()
+    stats_year = models.ForeignKey(StatisticYearly, blank=True, null=True)
 
     def update(self, daily_value):
         self.statistic_month += daily_value
-
-    def calculate_statistic(self):
-        pass
 
     def add_statistic_time(self, daily_stat):
         daily_stat.stats_month = self
@@ -45,13 +57,11 @@ class StatisticMonthly(StatisticTime):
 class StatisticDaily(StatisticTime):
     daily_donations = models.FloatField()
     donation_date = models.DateField(auto_now=True)
-    stats_month = models.ForeignKey(StatisticMonthly)
+    stats_month = models.ForeignKey(StatisticMonthly, blank=True, null=True)
 
     def update(self, donation_value):
         self.daily_donations += donation_value
 
-    def calculate_statistic(self):
-        pass
 
     def add_statistic_time(self):
         raise ValueError("You can't add a class to this object")
