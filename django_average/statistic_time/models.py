0352a618eaf37bc67630c99f3bd680b9ee2b142b
CaioNunes <caiofelipe147@gmail.com>
2017-11-24 03:27:01 -0200

    Defining monthly stats

--- django_average/statistic_time/models.py
+++ django_average/statistic_time/models.py
@@ -17,23 +17,47 @@ class StatisticTime(ObserverStatistic):
         pass
 
     def remove_statistic_time(self):
-        pass    
+        pass
 
-class StatisticDaily(StatisticTime):
-    statistic_day = models.FloatField()
-    donation_date = models.DateField(auto_now=True)
+class StatisticMonthly(StatisticTime):
+    # monthly_dontations = models.ForeignKey(StatisticDaily)
+    DAY_TOTAL = models.IntegerField() #number of days this month has
+    statistic_month = models.FloatField()
+    #stats_year = models.ForeignKey(StatisticYearly)
 
-    def get_statistic(self):
-        return self.statistic_day
-
-    def update(self, donation_value):
-        self.statistic_day += donation_value
+    def update(self, daily_value):
+        self.statistic_month += daily_value
 
     def calculate_statistic(self):
         pass
 
-    def add_statisc_time(self):
+    def add_statistic_time(self, daily_stat):
+        daily_stat.stats_month = self
+        self.update(daily_stat.daily_value)
+
+    def remove_statistic_time(self, daily_stat):
+        daily_stat.stats_month = None
+        self.statistic_month -= daily_stat.daily_value
+
+    def get_statistic(self):
+        return self.monthly_dontations
+
+class StatisticDaily(StatisticTime):
+    daily_donations = models.FloatField()
+    donation_date = models.DateField(auto_now=True)
+    stats_month = models.ForeignKey(StatisticMonthly)
+
+    def update(self, donation_value):
+        self.daily_donations += donation_value
+
+    def calculate_statistic(self):
+        pass
+
+    def add_statistic_time(self):
         raise ValueError("You can't add a class to this object")
 
     def remove_statistic_time(self):
         raise ValueError("There is no objects in this instance")
+
+    def get_statistic(self):
+        return self.daily_donations
